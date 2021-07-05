db5ea1b5b346d4eb3b189adb056d21bb45490b7e
손민 <80296020+thsals@users.noreply.github.com>
2021-07-05 18:35:17 +0900

    Create 리스트와벡터.R

---
+++ 리스트와벡터.R
@@ -0,0 +1,39 @@
+list1 <- list(c(1,2,3),"Hello")
+list1
+
+str(list1)
+
+list2 <- list(1:3,c("James","Litar"))
+
+list2
+
+list2 <- list(id = 1:3,names = c("James","Litar"))
+
+list2
+
+list2[[1]]
+list2[[2]]
+
+list2$id
+list2$names
+
+list2[1]
+list2[2]
+
+list3 <- list()
+list3 <- append(x,list(c(1,2,3)))
+list3 <- append(x,"result")
+list3
+
+
+x <- c(5,12,23,12)
+fa_x<- factor(x)
+fa_x
+
+str(fa_x)
+
+a <- c(1:10)
+b <- 21:25
+
+list6<-list(리스트1=c(1:10))
+list6
